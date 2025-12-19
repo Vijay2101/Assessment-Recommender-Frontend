b5d78c0c33dc3e265d351360096fa139dd5bb7de
Vijay Kumar <kumarvijay2003.vk@gmail.com>
2025-12-19 10:56:17 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://gen-ai-assessment-recommender.vercel.app/recommend", // FastAPI deployed URL
+  baseURL: "https://gen-ai-assessment-recommender.vercel.app/", // FastAPI deployed URL
 });
