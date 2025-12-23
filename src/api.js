33272cd825aea7dd22b3a97c0251150226a47233
Vijay Kumar <kumarvijay2003.vk@gmail.com>
2025-12-23 10:34:29 +0530

    Update API base URL to use environment variable

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://gen-ai-assessment-recommender.vercel.app/", // FastAPI deployed URL
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
